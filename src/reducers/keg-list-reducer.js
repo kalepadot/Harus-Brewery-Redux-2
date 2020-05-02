6290c0d8e0b6e423f9ed77257c4ed426a94ab041
Jeremy Kale Padot <jeremy@padot.us>
2020-05-02 15:50:12 -0700

    assign newkegslist to kegs

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -6,9 +6,9 @@ const currentState = {
   showHomePage: true,
   currentSelectedKeg: {},
   kegs: {
-    1: {name: "I'd Rather Be Smokin Cat Nip Pale Ale", brand: "Princess Haru's Brewery", image: BeerImage, price: "$4.20", alcoholPercent: "8%", inventory: 124, id: 1 },
+    1: {name: "I'd Rather Be Smokin Cat Nip Pale Ale", brand: "Princess Haru's Brewery", image: BeerImage, price: "$4.20", alcoholPercent: "8%", inventory: 124, id: 1
     2: {name: "Your Dog Sucks Milk Stout", brand: "Princess Haru's Brewery", image: FishImage, price: "$4.20", alcoholPercent: "8%", inventory: 124, id: 2},
-    3: { name: "No Dogs No Masters Lager", brand: "Princess Haru's Brewery", image: Woof, price: "$4.20", alcoholPercent: "666%", inventory: 124, id: 3}
+    3: { name: "No Dogs No Masters Lager", brand: "Princess Haru's Brewery", image: Woof, price: "$4.20", alcoholPercent: "666%", inventory: 124, id: 3} },
   }
 };
 
@@ -16,7 +16,7 @@ export default (state = currentState, action) => {
   const { name, brand, image, price, alcoholPercent, inventory, id } = action;
   switch (action.type) {
   case 'ADD_KEG':
-    return Object.assign({}, state, {
+    const newKegsList = Object.assign({}, state.kegs, {
       [id]: {
         name: name,
         brand: brand,
@@ -26,7 +26,10 @@ export default (state = currentState, action) => {
         inventory: inventory,
         id: id
       }
-    });
+    })
+
+    return  { ...state, kegs: newKegsList };
+    
     case 'DELETE_KEG':
       const newState = { ...state };
       delete newState[id];
